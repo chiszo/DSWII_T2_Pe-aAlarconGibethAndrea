5ee8588fb3ccd5e029b1f787c1215f4896e3486e
chiszo <gigibethandrea@gmail.com>
2024-06-23 12:08:57 -0500

    2.0

--- src/main/java/pe/edu/cibertec/T2_DSWII/controller/AutorController.java
+++ src/main/java/pe/edu/cibertec/T2_DSWII/controller/AutorController.java
@@ -1,4 +1,61 @@
 package pe.edu.cibertec.T2_DSWII.controller;
 
+import lombok.AllArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import pe.edu.cibertec.T2_DSWII.exception.ResourceNotFoundException;
+import pe.edu.cibertec.T2_DSWII.model.bd.Autor;
+import pe.edu.cibertec.T2_DSWII.service.AutorService;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@AllArgsConstructor
+@RestController
+@RequestMapping(path = "api/v1/autor")
 public class AutorController {
+
+    private AutorService autorService;
+
+    @GetMapping("")
+    public ResponseEntity<List<Autor>> listarAutores(){
+        List<Autor> autorList = new ArrayList<>(autorService.listarAutores());
+        if(autorList.isEmpty()){
+            return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+        }
+        return new ResponseEntity<>(autorList, HttpStatus.OK);
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Autor> obtenerautorXId(
+            @PathVariable Integer id){
+        Autor autor = autorService
+                .obtenerAutorxId(id).orElseThrow(
+                        () -> new ResourceNotFoundException("La Autor con Id " +
+                                id + " no existe"));
+        return new ResponseEntity<>(autor, HttpStatus.OK);
+    }
+
+    @PostMapping("")
+    public ResponseEntity<Autor> registrarautor(
+            @RequestBody Autor autor
+    ){
+        return new ResponseEntity<>(autorService.guardarAutor(autor),HttpStatus.CREATED);
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<Autor> actualizarcategoria(
+            @PathVariable Integer id,
+            @RequestBody Autor autor
+    ){
+        Autor newautor = autorService.obtenerAutorxId(id)
+                .orElseThrow(() -> new ResourceNotFoundException("La autor con ID"
+                        + id + " no existe"));
+        newautor.setNomautor(autor.getNomautor());
+        newautor.setApeautor(autor.getApeautor());
+        newautor.setFechNacAutor(autor.getFechNacAutor());
+        return new ResponseEntity<>(autorService.guardarAutor(newautor),
+                HttpStatus.OK);
+    }
 }
--- src/main/java/pe/edu/cibertec/T2_DSWII/controller/PublicacionController.java
+++ src/main/java/pe/edu/cibertec/T2_DSWII/controller/PublicacionController.java
@@ -1,4 +1,62 @@
 package pe.edu.cibertec.T2_DSWII.controller;
 
+import lombok.AllArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import pe.edu.cibertec.T2_DSWII.exception.ResourceNotFoundException;
+import pe.edu.cibertec.T2_DSWII.model.bd.Publicacion;
+import pe.edu.cibertec.T2_DSWII.service.PublicacionService;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@AllArgsConstructor
+@RestController
+@RequestMapping(path = "api/v1/publicacion")
 public class PublicacionController {
+
+    private PublicacionService publicacionService;
+
+    @GetMapping("")
+    public ResponseEntity<List<Publicacion>> listarPublicaciones(){
+        List<Publicacion> publicacionList = new ArrayList<>(publicacionService.listarPublicaciones());
+        if(publicacionList.isEmpty()){
+            return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+        }
+        return new ResponseEntity<>(publicacionList, HttpStatus.OK);
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Publicacion> obtenerPublicacionXId(
+            @PathVariable Integer id){
+        Publicacion publicacion = publicacionService
+                .obtenerPublicacionxId(id).orElseThrow(
+                        () -> new ResourceNotFoundException("La Publicacion con Id " +
+                                id + " no existe"));
+        return new ResponseEntity<>(publicacion, HttpStatus.OK);
+    }
+
+    @PostMapping("")
+    public ResponseEntity<Publicacion> registrarCategoria(
+            @RequestBody Publicacion publicacion
+    ){
+        return new ResponseEntity<>(publicacionService.guardarPublicacion(publicacion),HttpStatus.CREATED);
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<Publicacion> actualizarcategoria(
+            @PathVariable Integer id,
+            @RequestBody Publicacion publicacion
+    ){
+        Publicacion newpublicacion = publicacionService.obtenerPublicacionxId(id)
+                .orElseThrow(() -> new ResourceNotFoundException("La publicacion con ID"
+                        + id + " no existe"));
+        newpublicacion.setTitulo(publicacion.getTitulo());
+        newpublicacion.setResumen(publicacion.getResumen());
+        newpublicacion.setFechapublicacion(publicacion.getFechapublicacion());
+        newpublicacion.setIdautor(publicacion.getIdautor());
+        return new ResponseEntity<>(publicacionService.guardarPublicacion(newpublicacion),
+                HttpStatus.OK);
+    }
 }
