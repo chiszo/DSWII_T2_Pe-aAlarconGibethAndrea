ecb3fec1b2ca724555b732dd0bdafe4329bb596a
chiszo <gigibethandrea@gmail.com>
2024-06-23 11:08:30 -0500

    subir de creacion de bd

---
+++ bd/grupo4.sql
@@ -0,0 +1,19 @@
+CREATE TABLE Autor
+(
+ IdAutor 		INT NOT NULL AUTO_INCREMENT,
+ NomAutor		VARCHAR(50) NOT NULL,
+ ApeAutor 		VARCHAR(50) NOT NULL,
+ FechNacAutor	DATE NOT NULL,
+ PRIMARY KEY (IdAutor)
+);
+
+CREATE TABLE Publicacion
+(
+ IdPublicacion 		INT NOT NULL AUTO_INCREMENT,
+ Titulo 			VARCHAR(250) NOT NULL,
+ Resumen 			VARCHAR(250) NOT NULL,
+ FechPublicacion	DATE NOT NULL,
+ IdAutor 			INT NOT NULL,
+ PRIMARY KEY (IdPublicacion),
+ FOREIGN KEY (IdAutor) REFERENCES Autor(IdAutor)
+ );
