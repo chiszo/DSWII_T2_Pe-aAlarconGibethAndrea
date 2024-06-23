618bd57c772cd3f6b2dbb48229670e11afaacedb
chiszo <gigibethandrea@gmail.com>
2024-06-23 08:57:31 -0500

    creacion del proyecto

---
+++ src/main/java/pe/edu/cibertec/T2_DSWII/T2DswiiApplication.java
@@ -0,0 +1,13 @@
+package pe.edu.cibertec.T2_DSWII;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class T2DswiiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(T2DswiiApplication.class, args);
+	}
+
+}
